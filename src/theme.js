5737aaca631aa016d82e4fdd4842030e354e5c85
Pawel Majka <pawelmajka1989@gmail.com>
2023-09-30 15:44:21 +0200

    added styled components theme.js

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+    color: {
+        black: "#000",
+        teal: "#008080",
+        alto: "#ddd",
+        white: "#fff",
+        silver: "ccc",
+        gallery: "#eee",
+        guardsmanRed: "#CC0000",
+        forestGreen: "#228c22",
+    },
+
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
